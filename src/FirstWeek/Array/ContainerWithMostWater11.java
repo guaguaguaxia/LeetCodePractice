71cd33d33a159148c8fad85ddc1ff11dd0c0869d
guaguaguaxia <1030056125@qq.com>
2020-06-23 22:19:59 +0800

    ContainerWithMostWater11  2020-06-23 First Pass MoveZeroes283 2020-06-23 Second Pass

---
+++ src/FirstWeek/Array/ContainerWithMostWater11.java
@@ -0,0 +1,62 @@
+package FirstWeek.Array;
+
+
+
+/*
+* 2020-06-23 First Pass
+*
+* */
+public class ContainerWithMostWater11 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int ret = maxArea2(nums);
+        System.out.println(ret);
+    }
+
+
+    /**
+     * 双指针
+     * 定义双指针，两边向里逼近，谁的高小谁向里逼近
+     * @param height
+     * @return
+     */
+    public static int maxArea2(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int maxArea = 0;
+        while (i < j) {
+            if (height[i] < height[j]) {
+                maxArea = Math.max(maxArea, (j - i) * height[i]);
+                i++;
+            } else {
+                maxArea = Math.max(maxArea, (j - i) * height[j]);
+                j--;
+            }
+        }
+        return maxArea;
+    }
+
+
+    /**
+     * 暴力解法
+     *
+     * @param height
+     * @return
+     */
+    private static int maxArea1(int[] height) {
+        if (height.length < 2) {
+            return 0;
+        }
+        int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                int area = height[i] < height[j] ? (j - i) * height[i] : (j - i) * height[j];
+                if (area > maxArea) {
+                    maxArea = area;
+                }
+            }
+        }
+        return maxArea;
+    }
+}
--- src/FirstWeek/Array/MoveZeroes283.java
+++ src/FirstWeek/Array/MoveZeroes283.java
@@ -2,32 +2,34 @@ package FirstWeek.Array;
 
 
 /*
- * 2020-06-22
- *
+ * 2020-06-22 First Pass
+ * 2020-06-23 Second Pass
  * */
 
-/*
-* 定义快慢指针i、j,由i指针去寻找需要调换的不等于0的元素，和j指针元素调换
-* */
+
+import java.util.Arrays;
+
 public class MoveZeroes283 {
     public static void main(String[] args) {
         int[] nums = {1, 1, 0, 0, 0, 3, 12};
         moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
     }
 
+    /**
+     * 定义快慢指针i、j,由i指针去寻找需要调换的不等于0的元素，和j指针元素调换
+     */
     private static void moveZeroes(int[] nums) {
         if (nums == null) {
             return;
         }
-        //两个指针i和j
         int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            //当前元素!=0，就把其交换到左边，等于0的交换到右边
-            if (nums[i] != 0) {
-                int tmp = nums[i];
+        for (int i = 0; i < nums.length;i ++){
+            if(nums[i] != 0){
+                int temp = nums[i];
                 nums[i] = nums[j];
-                nums[j] = tmp;
-                j++;
+                nums[j] = temp;
+                j ++;
             }
         }
     }
