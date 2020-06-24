819d04fc937b548220e8f255210b793dc8460096
guaguaguaxia <1030056125@qq.com>
2020-06-24 23:24:28 +0800

    MoveZeroes283  2020-06-24 Third Pass ContainerWithMostWater11  2020-06-24 Second Pass

--- src/FirstWeek/Array/ContainerWithMostWater11.java
+++ src/FirstWeek/Array/ContainerWithMostWater11.java
@@ -4,7 +4,7 @@ package FirstWeek.Array;
 
 /*
 * 2020-06-23 First Pass
-*
+* 2020-06-24 Second Pass
 * */
 public class ContainerWithMostWater11 {
 
@@ -25,13 +25,13 @@ public class ContainerWithMostWater11 {
         int i = 0;
         int j = height.length - 1;
         int maxArea = 0;
-        while (i < j) {
-            if (height[i] < height[j]) {
-                maxArea = Math.max(maxArea, (j - i) * height[i]);
-                i++;
-            } else {
-                maxArea = Math.max(maxArea, (j - i) * height[j]);
+        while (i < j){
+            if (height[i] > height[j]){
+                maxArea = Math.max(maxArea,(j - i) * height[j]);
                 j--;
+            }else {
+                maxArea = Math.max(maxArea,(j - i) * height[i]);
+                i++;
             }
         }
         return maxArea;
--- src/FirstWeek/Array/MoveZeroes283.java
+++ src/FirstWeek/Array/MoveZeroes283.java
@@ -4,6 +4,7 @@ package FirstWeek.Array;
 /*
  * 2020-06-22 First Pass
  * 2020-06-23 Second Pass
+ * 2020-06-24 Third Pass
  * */
 
 
@@ -24,12 +25,12 @@ public class MoveZeroes283 {
             return;
         }
         int j = 0;
-        for (int i = 0; i < nums.length;i ++){
-            if(nums[i] != 0){
+        for(int i = 0;i < nums.length;i++){
+            if (nums[i] != 0){
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                j ++;
+                j++;
             }
         }
     }
