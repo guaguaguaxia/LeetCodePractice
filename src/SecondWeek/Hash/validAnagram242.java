50b120358a399076ba393b5b84f4406c0e74542d
guaguaguaxia <guaguaguaxia@Gmail.com>
2020-07-13 22:02:41 +0800

    2020-07-13

---
+++ src/SecondWeek/Hash/validAnagram242.java
@@ -0,0 +1,28 @@
+package SecondWeek.Hash;
+
+public class validAnagram242 {
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        isAnagram(s,t);
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] alpha = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            alpha[s.charAt(i) - 'a']++;
+            alpha[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (alpha[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/SecondWeek/test.java
@@ -0,0 +1,4 @@
+package SecondWeek;
+
+public class test {
+}
