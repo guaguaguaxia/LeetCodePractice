7f41cb6ca11230cc49257d18b6189774a08aa7fc
guaguaguaxia <guaguaguaxia@Gmail.com>
2020-07-21 17:24:58 +0800

    2020-07-21

--- src/SecondWeek/Recursive/GenerateParentheses22.java
+++ src/SecondWeek/Recursive/GenerateParentheses22.java
@@ -10,30 +10,54 @@ public class GenerateParentheses22 {
         GenerateParentheses22 ob = new GenerateParentheses22();
         List<String> generate = ob.generateParenthesis(3);
         generate.forEach(System.out::println);
-    }
 
-    public List<String> generate(int n) {
-        if (cache[n] != null) {
-            return cache[n];
-        }
-        ArrayList<String> ans = new ArrayList();
-        if (n == 0) {
-            ans.add("");
-        } else {
-            for (int c = 0; c < n; ++c) {
-                for (String left : generate(c)) {
-                    for (String right : generate(n - 1 - c)) {
-                        ans.add("(" + left + ")" + right);
-                    }
-                }
-            }
-        }
-        cache[n] = ans;
-        return ans;
     }
 
     public List<String> generateParenthesis(int n) {
-        return generate(n);
+        List<String> ans = new ArrayList();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
     }
+
+    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max){
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+
+        if (open < max) {
+            backtrack(ans, cur.append("("), open+1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (open > close) {
+            backtrack(ans, cur.append(")"), open, close+1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+
+
+//    public List<String> generate(int n) {
+//        if (cache[n] != null) {
+//            return cache[n];
+//        }
+//        ArrayList<String> ans = new ArrayList();
+//        if (n == 0) {
+//            ans.add("");
+//        } else {
+//            for (int c = 0; c < n; ++c) {
+//                for (String left : generate(c)) {
+//                    for (String right : generate(n - 1 - c)) {
+//                        ans.add("(" + left + ")" + right);
+//                    }
+//                }
+//            }
+//        }
+//        cache[n] = ans;
+//        return ans;
+//    }
+//
+//    public List<String> generateParenthesis(int n) {
+//        return generate(n);
+//    }
 
 }
--- src/SecondWeek/Recursive/ValidateBinarySearchTree98.java
+++ src/SecondWeek/Recursive/ValidateBinarySearchTree98.java
@@ -5,10 +5,35 @@ import java.util.Stack;
 public class ValidateBinarySearchTree98 {
 
     public static void main(String[] args) {
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode4 = new TreeNode(4);
+        TreeNode treeNode5 = new TreeNode(5);
+        TreeNode treeNode6 = new TreeNode(6);
+        TreeNode treeNode7 = new TreeNode(7);
 
+        treeNode4.left = treeNode3;
+        treeNode3.left = treeNode2;
+        treeNode2.left = treeNode1;
+
+        treeNode4.right = treeNode5;
+        treeNode5.right = treeNode6;
+        treeNode6.right = treeNode7;
+
+        boolean validBST = isValidBST(treeNode4);
+        System.out.println(validBST);
+
+        /*
+         *            4
+         *         3     5
+         *      2           6
+         *   1                 7
+         *
+         * */
     }
 
-    public boolean isValidBST(TreeNode root) {
+    public static boolean isValidBST(TreeNode root) {
         Stack<TreeNode> stack = new Stack();
         double inorder = -Double.MAX_VALUE;
 
@@ -29,7 +54,7 @@ public class ValidateBinarySearchTree98 {
     }
 
 
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
