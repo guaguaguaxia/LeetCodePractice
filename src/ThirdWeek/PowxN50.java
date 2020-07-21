d774c714d105904fc71d8368169a2e83278144ba
guaguaguaxia <1030056125@qq.com>
2020-07-21 22:53:41 +0800

    2020-07-21

--- src/ThirdWeek/PowxN50.java
+++ src/ThirdWeek/PowxN50.java
@@ -2,11 +2,25 @@ package ThirdWeek;
 
 public class PowxN50 {
     public static void main(String[] args) {
-        double v = myPow(5, 2);
+        double v = myPow1(5, 2);
         System.out.println(v);
     }
 
-    public static double myPow(double x, int n) {
+    public static double quickMul(double x, long N) {
+        if (N == 0) {
+            return 1.0;
+        }
+        double y = quickMul(x, N / 2);
+        return N % 2 == 0 ? y * y : y * y * x;
+    }
+
+    public static double myPow1(double x, int n) {
+        long N = n;
+        return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+
+
+    public static double myPow2(double x, int n) {
         if (x == 0.0f) {
             return 0.0d;
         }
