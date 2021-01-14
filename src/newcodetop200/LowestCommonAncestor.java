370603cdf66cae843b327298e7e03b99a77fdb15
guaguaguaxia <guaguaguaxia@Gmail.com>
2021-01-14 18:03:01 +0800

    2021-01-14

---
+++ src/newcodetop200/LowestCommonAncestor.java
@@ -0,0 +1,60 @@
+package newcodetop200;
+
+public class LowestCommonAncestor {
+
+    public static void main(String[] args) {
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode4 = new TreeNode(4);
+        TreeNode treeNode5 = new TreeNode(5);
+        TreeNode treeNode6 = new TreeNode(6);
+        TreeNode treeNode7 = new TreeNode(7);
+        TreeNode treeNode8 = new TreeNode(8);
+        TreeNode treeNode9 = new TreeNode(9);
+        treeNode1.left = treeNode2;
+        treeNode1.right = treeNode3;
+        treeNode2.left = treeNode4;
+        treeNode2.right = treeNode5;
+        treeNode5.left = treeNode7;
+        treeNode3.right = treeNode6;
+        treeNode6.left = treeNode8;
+        treeNode6.right = treeNode9;
+
+        TreeNode treeNode = lowestCommonAncestor(treeNode1, treeNode2, treeNode3);
+        System.out.println(treeNode);
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // base case
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // 情况 1
+        if (left != null && right != null) {
+            return root;
+        }
+        // 情况 2
+        if (left == null && right == null) {
+            return null;
+        }
+        // 情况 3
+        return left == null ? right : left;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
