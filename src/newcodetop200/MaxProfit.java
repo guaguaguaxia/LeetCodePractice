8416181522c17e8b89826ae0ea7a14d0d96b4804
guaguaguaxia <guaguaguaxia@Gmail.com>
2021-01-14 11:21:12 +0800

    2021-01-14

---
+++ src/newcodetop200/MaxProfit.java
@@ -0,0 +1,47 @@
+package newcodetop200;
+
+public class MaxProfit {
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        int i = maxProfit_K_1(prices);
+        System.out.println(i);
+    }
+
+    static int maxProfit_K_1(int[] prices) {
+        int n = prices.length;
+        // base case: dp[-1][0] = 0, dp[-1][1] = -infinity
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            // dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            // dp[i][1] = max(dp[i-1][1], -prices[i])
+            dp_i_1 = Math.max(dp_i_1, -prices[i]);
+        }
+        return dp_i_0;
+    }
+
+    int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i - 1 == -1) {
+                dp[i][0] = 0;
+                // 解释：
+                //   dp[i][0]
+                // = max(dp[-1][0], dp[-1][1] + prices[i])
+                // = max(0, -infinity + prices[i]) = 0
+                dp[i][1] = -prices[i];
+                //解释：
+                //   dp[i][1]
+                // = max(dp[-1][1], dp[-1][0] - prices[i])
+                // = max(-infinity, 0 - prices[i])
+                // = -prices[i]
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+}
