540eeec842d811fa96a419babd12c78d7802b0e9
guaguaguaxia <1030056125@qq.com>
2021-01-14 21:55:16 +0800

    2021-01-14

---
+++ src/newcodetop200/GetIntersectionNode.java
@@ -0,0 +1,39 @@
+package newcodetop200;
+
+public class GetIntersectionNode {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode ha = headA, hb = headB;
+        while (ha != hb) {
+            if (ha != null) {
+                ha = ha.next;
+            } else {
+                ha = headB;
+            }
+            if (hb != null) {
+                hb = hb.next;
+            } else {
+                hb = headA;
+            }
+        }
+        return ha;
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
---
+++ src/newcodetop200/MaxSubArray.java
@@ -0,0 +1,19 @@
+package newcodetop200;
+
+public class MaxSubArray {
+
+    public int maxSubArray(int[] nums) {
+        int ans = nums[0];
+        int sum = 0;
+        for(int num: nums) {
+            if(sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+    }
+
+}
