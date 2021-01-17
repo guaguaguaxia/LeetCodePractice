0f96e9d82b9e39f307418278f0e24ae1f8097e2d
guaguaguaxia <1030056125@qq.com>
2021-01-17 23:13:56 +0800

    2021-01-17

--- src/newcodetop200/part2/MaxPathSum.java
+++ src/newcodetop200/part2/MaxPathSum.java
@@ -2,14 +2,42 @@ package newcodetop200.part2;
 
 public class MaxPathSum {
 
-    int maxSum = Integer.MIN_VALUE;
+    public static void main(String[] args) {
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode4 = new TreeNode(4);
+        TreeNode treeNode5 = new TreeNode(5);
+        TreeNode treeNode6 = new TreeNode(6);
+        TreeNode treeNode7 = new TreeNode(7);
+        TreeNode treeNode8 = new TreeNode(8);
+        TreeNode treeNode9 = new TreeNode(9);
+        treeNode1.left = treeNode2;
+        treeNode1.right = treeNode3;
+        treeNode2.left = treeNode4;
+        treeNode2.right = treeNode5;
+        treeNode5.left = treeNode7;
+        treeNode3.right = treeNode6;
+        treeNode6.left = treeNode8;
+        treeNode6.right = treeNode9;
+        int i = maxPathSum(treeNode1);
+        /*
+         *       1
+         *    2     3
+         * 4    5     6
+         *    7     8   9
+         * */
+        System.out.println(i);
+    }
 
-    public int maxPathSum(TreeNode root) {
+    static int maxSum = Integer.MIN_VALUE;
+
+    public static int maxPathSum(TreeNode root) {
         maxGain(root);
         return maxSum;
     }
 
-    public int maxGain(TreeNode node) {
+    public static int maxGain(TreeNode node) {
         if (node == null) {
             return 0;
         }
--- src/newcodetop200/part2/MaximumDepthOfBinaryTree104.java
+++ src/newcodetop200/part2/MaximumDepthOfBinaryTree104.java
@@ -2,10 +2,34 @@ package newcodetop200.part2;
 
 public class MaximumDepthOfBinaryTree104 {
     public static void main(String[] args) {
-
+       TreeNode treeNode1 = new TreeNode(1);
+       TreeNode treeNode2 = new TreeNode(2);
+       TreeNode treeNode3 = new TreeNode(3);
+       TreeNode treeNode4 = new TreeNode(4);
+       TreeNode treeNode5 = new TreeNode(5);
+       TreeNode treeNode6 = new TreeNode(6);
+       TreeNode treeNode7 = new TreeNode(7);
+       TreeNode treeNode8 = new TreeNode(8);
+       TreeNode treeNode9 = new TreeNode(9);
+        treeNode1.left = treeNode2;
+        treeNode1.right = treeNode3;
+        treeNode2.left = treeNode4;
+        treeNode2.right = treeNode5;
+        treeNode5.left = treeNode7;
+        treeNode3.right = treeNode6;
+        treeNode6.left = treeNode8;
+        treeNode6.right = treeNode9;
+        int i = maxDepth(treeNode1);
+        System.out.println(i);
+        /*
+         *       1
+         *    2     3
+         * 4    5     6
+         *    7     8   9
+         * */
     }
 
-    public int maxDepth(TreeNode root) {
+    public static int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
@@ -15,7 +39,7 @@ public class MaximumDepthOfBinaryTree104 {
     }
 
 
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
