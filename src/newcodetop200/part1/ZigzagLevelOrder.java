b90d34bf1a4bcceb47b20d02f0c86d1a6c0afafa
guaguaguaxia <1030056125@qq.com>
2021-01-17 15:43:32 +0800

    2021-01-17

--- src/newcodetop200/part1/ZigzagLevelOrder.java
+++ src/newcodetop200/part1/ZigzagLevelOrder.java
@@ -7,7 +7,35 @@ import java.util.Queue;
 
 public class ZigzagLevelOrder {
 
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+    public static void main(String[] args) {
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode4 = new TreeNode(4);
+        TreeNode treeNode5 = new TreeNode(5);
+        TreeNode treeNode6 = new TreeNode(6);
+        TreeNode treeNode7 = new TreeNode(7);
+        TreeNode treeNode8 = new TreeNode(8);
+        TreeNode treeNode9 = new TreeNode(9);
+        treeNode1.left = treeNode2;
+        treeNode1.right = treeNode3;
+        treeNode2.left = treeNode4;
+        treeNode2.right = treeNode5;
+        treeNode5.left = treeNode7;
+        treeNode3.right = treeNode6;
+        treeNode6.left = treeNode8;
+        treeNode6.right = treeNode9;
+        List<List<Integer>> lists = zigzagLevelOrder(treeNode1);
+        System.out.println(lists);
+        /*
+         *       1
+         *    2     3
+         * 4    5     6
+         *    7     8   9
+         * */
+    }
+
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null)
             return res;
