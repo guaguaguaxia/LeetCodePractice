e8b89ffc97ae13629d1a8bda687c0f772f9a1233
guaguaguaxia <1030056125@qq.com>
2021-01-17 23:23:27 +0800

    2021-01-17

--- src/newcodetop200/part2/MergeSortedArray.java
+++ src/newcodetop200/part2/MergeSortedArray.java
@@ -3,22 +3,15 @@ package newcodetop200.part2;
 public class MergeSortedArray {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // two get pointers for nums1 and nums2
-        int p1 = m - 1;
-        int p2 = n - 1;
-        // set pointer for nums1
-        int p = m + n - 1;
-
-        // while there are still elements to compare
-        while ((p1 >= 0) && (p2 >= 0)){
-            // compare two elements from nums1 and nums2
-            // and add the largest one in nums1
-            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+        int len1 = m - 1;
+        int len2 = n - 1;
+        int len = m + n - 1;
+        while(len1 >= 0 && len2 >= 0) {
+            // 注意--符号在后面，表示先进行计算再减1，这种缩写缩短了代码
+            nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
         }
-
-
-        // add missing elements from nums2
-        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+        // 表示将nums2数组从下标0位置开始，拷贝到nums1数组中，从下标0位置开始，长度为len2+1
+        System.arraycopy(nums2, 0, nums1, 0, len2 + 1);
     }
 
 }
