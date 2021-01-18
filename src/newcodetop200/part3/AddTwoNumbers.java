d0e93a4542680a5e0f7763bff6e43a122b67a0b9
guaguaguaxia <1030056125@qq.com>
2021-01-18 23:33:32 +0800

    2021-01-18

---
+++ src/newcodetop200/part3/AddTwoNumbers.java
@@ -0,0 +1,75 @@
+package newcodetop200.part3;
+
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode1 = new ListNode(1);
+
+        listNode3.next = listNode2;
+        listNode2.next = listNode1;
+
+        ListNode listNode6 = new ListNode(6);
+        ListNode listNode5 = new ListNode(5);
+        ListNode listNode4 = new ListNode(4);
+
+        listNode6.next = listNode5;
+        listNode5.next = listNode4;
+
+        AddTwoNumbers add = new AddTwoNumbers();
+        ListNode listNode = add.addTwoNumbers(listNode3, listNode6);
+
+        while (listNode != null){
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+    }
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int carry = 0;
+        while(l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            /*进位*/
+            carry = sum > 9 ? 1 : 0;
+            /*相加之后的单数*/
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+
+            cur = cur.next;
+            if(l1 != null){
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                l2 = l2.next;
+            }
+        }
+        if(carry == 1) {
+            cur.next = new ListNode(carry);
+        }
+        return pre.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
--- src/newcodetop200/part3/UniquePaths.java
+++ src/newcodetop200/part3/UniquePaths.java
@@ -1,10 +1,15 @@
-package interview;
+package newcodetop200.part3;
 
 import java.util.Arrays;
 
 public class UniquePaths {
 
-    public int uniquePaths(int m, int n) {
+    public static void main(String[] args) {
+        int i = uniquePaths(3, 7);
+        System.out.println(i);
+    }
+
+    public static int uniquePaths(int m, int n) {
         int[] cur = new int[n];
         Arrays.fill(cur,1);
         for (int i = 1; i < m;i++){
