cba1228362c70d7eb499a0480c3ef81dc113e89d
guaguaguaxia <1030056125@qq.com>
2021-01-19 23:36:03 +0800

    2021-01-19

---
+++ src/newcodetop200/part4/ReorderList.java
@@ -0,0 +1,51 @@
+package newcodetop200.part4;
+
+import newcodetop200.part3.DeleteDuplicates;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReorderList {
+
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        List<ListNode> list = new ArrayList<ListNode>();
+        ListNode node = head;
+        while (node != null) {
+            list.add(node);
+            node = node.next;
+        }
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            list.get(i).next = list.get(j);
+            i++;
+            if (i == j) {
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--;
+        }
+        list.get(i).next = null;
+    }
+
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
