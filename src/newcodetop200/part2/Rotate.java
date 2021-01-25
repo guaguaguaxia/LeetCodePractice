fe3207c4447651422e2d570585b9550a0937b7e2
guaguaguaxia <guaguaguaxia@Gmail.com>
2021-01-25 17:13:08 +0800

    2021-01-25

--- src/newcodetop200/part2/Rotate.java
+++ src/newcodetop200/part2/Rotate.java
@@ -4,21 +4,20 @@ public class Rotate {
 
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-
-        // transpose matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                int tmp = matrix[j][i];
-                matrix[j][i] = matrix[i][j];
-                matrix[i][j] = tmp;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
             }
         }
-        // reverse each row
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n / 2; j++) {
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[i][n - j - 1];
-                matrix[i][n - j - 1] = tmp;
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
     }
