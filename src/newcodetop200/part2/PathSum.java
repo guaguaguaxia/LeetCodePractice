91eec05a8f379f627e3da15632e3f1d3f65e6c78
guaguaguaxia <guaguaguaxia@Gmail.com>
2021-01-29 14:43:15 +0800

    2021-01-29

--- src/newcodetop200/part2/PathSum.java
+++ src/newcodetop200/part2/PathSum.java
@@ -3,11 +3,12 @@ package newcodetop200.part2;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class PathSum {
 
     List<List<Integer>> ret = new LinkedList<List<Integer>>();
-    Deque<Integer> path = new LinkedList<Integer>();
+    Stack<Integer> path = new Stack<Integer>();
 
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
         dfs(root, sum);
@@ -18,14 +19,14 @@ public class PathSum {
         if (root == null) {
             return;
         }
-        path.offerLast(root.val);
+        path.add(root.val);
         sum = sum - root.val;
         if (root.left == null && root.right == null && sum == 0) {
             ret.add(new LinkedList<Integer>(path));
         }
         dfs(root.left, sum);
         dfs(root.right, sum);
-        path.pollLast();
+        path.pop();
     }
 
     public static class TreeNode {
