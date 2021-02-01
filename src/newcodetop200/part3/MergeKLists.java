f187061a3c13ef1c17fc97e0532b03749a22f359
guaguaguaxia <guaguaguaxia@Gmail.com>
2021-02-01 10:48:32 +0800

    2021-02-01

--- src/newcodetop200/part3/MergeKLists.java
+++ src/newcodetop200/part3/MergeKLists.java
@@ -55,8 +55,12 @@ public class MergeKLists {
     }
 
     private static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
         if (l1.val < l2.val) {
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
