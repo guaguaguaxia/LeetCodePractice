b37a7c24a2ae0d44c42f469f4b5eb151b241686c
guaguaguaxia <guaguaguaxia@Gmail.com>
2021-02-01 15:28:46 +0800

    2021-02-01

--- src/newcodetop200/part3/RestoreIpAddresses.java
+++ src/newcodetop200/part3/RestoreIpAddresses.java
@@ -11,54 +11,76 @@ public class RestoreIpAddresses {
 
     }
 
-    static final int SEG_COUNT = 4;
-    List<String> ans = new ArrayList<String>();
-    int[] segments = new int[SEG_COUNT];
-
     public List<String> restoreIpAddresses(String s) {
-        segments = new int[SEG_COUNT];
-        dfs(s, 0, 0);
-        return ans;
+        int len = s.length();
+        List<String> res = new ArrayList<>();
+        // 如果长度不够，不搜索
+        if (len < 4 || len > 12) {
+            return res;
+        }
+
+        Deque<String> path = new ArrayDeque<>(4);
+        int splitTimes = 0;
+        dfs(s, len, splitTimes, 0, path, res);
+        return res;
     }
 
-    public void dfs(String s, int segId, int segStart) {
-        // 如果找到了 4 段 IP 地址并且遍历完了字符串，那么就是一种答案
-        if (segId == SEG_COUNT) {
-            if (segStart == s.length()) {
-                StringBuffer ipAddr = new StringBuffer();
-                for (int i = 0; i < SEG_COUNT; ++i) {
-                    ipAddr.append(segments[i]);
-                    if (i != SEG_COUNT - 1) {
-                        ipAddr.append('.');
-                    }
-                }
-                ans.add(ipAddr.toString());
+    private void dfs(String s, int len, int split, int begin, Deque<String> path, List<String> res) {
+        if (begin == len) {
+            if (split == 4) {
+                res.add(String.join(".", path));
             }
             return;
         }
 
-        // 如果还没有找到 4 段 IP 地址就已经遍历完了字符串，那么提前回溯
-        if (segStart == s.length()) {
+        // 看到剩下的不够了，就退出（剪枝），len - begin 表示剩余的还未分割的字符串的位数
+        if (len - begin < (4 - split) || len - begin > 3 * (4 - split)) {
             return;
         }
 
-        // 由于不能有前导零，如果当前数字为 0，那么这一段 IP 地址只能为 0
-        if (s.charAt(segStart) == '0') {
-            segments[segId] = 0;
-            dfs(s, segId + 1, segStart + 1);
-        }
-
-        // 一般情况，枚举每一种可能性并递归
-        int addr = 0;
-        for (int segEnd = segStart; segEnd < s.length(); ++segEnd) {
-            addr = addr * 10 + (s.charAt(segEnd) - '0');
-            if (addr > 0 && addr <= 0xFF) {
-                segments[segId] = addr;
-                dfs(s, segId + 1, segEnd + 1);
-            } else {
+        for (int i = 0; i < 3; i++) {
+            if (begin + i >= len) {
                 break;
             }
+
+            int ipSegment = judgeIfIpSegment(s, begin, begin + i);
+            if (ipSegment != -1) {
+                // 在判断是 ip 段的情况下，才去做截取
+                path.addLast(ipSegment + "");
+                dfs(s, len, split + 1, begin + i + 1, path, res);
+                path.removeLast();
+            }
         }
     }
+
+    /**
+     * 判断 s 的子区间 [left, right] 是否能够成为一个 ip 段
+     * 判断的同时顺便把类型转了
+     *
+     * @param s
+     * @param left
+     * @param right
+     * @return
+     */
+    private int judgeIfIpSegment(String s, int left, int right) {
+        int len = right - left + 1;
+
+        // 大于 1 位的时候，不能以 0 开头
+        if (len > 1 && s.charAt(left) == '0') {
+            return -1;
+        }
+
+        // 转成 int 类型
+        int res = 0;
+        for (int i = left; i <= right; i++) {
+            res = res * 10 + s.charAt(i) - '0';
+        }
+
+        if (res > 255) {
+            return -1;
+        }
+        return res;
+    }
+
 
 }
