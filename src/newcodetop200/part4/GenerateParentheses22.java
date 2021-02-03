7e5fca4eac1c844ae77f1df67532d66e79805602
guaguaguaxia <guaguaguaxia@Gmail.com>
2021-02-03 11:07:44 +0800

    2021-02-03

--- src/newcodetop200/part4/GenerateParentheses22.java
+++ src/newcodetop200/part4/GenerateParentheses22.java
@@ -14,25 +14,44 @@ public class GenerateParentheses22 {
     }
 
     public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList();
-        backtrack(ans, new StringBuilder(), 0, 0, n);
-        return ans;
+        if (n == 0) {
+            return null;
+        }
+        // 记录所有合法的括号组合
+        List<String> res = new ArrayList<>();
+        // 回溯过程中的路径
+        StringBuilder track = new StringBuilder();
+        // 可用的左括号和右括号数量初始化为 n
+        backtrack(n, n, track, res);
+        return res;
     }
 
-    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max){
-        if (cur.length() == max * 2) {
-            ans.add(cur.toString());
+    // 可用的左括号数量为 left 个，可用的右括号数量为 rgiht 个
+    void backtrack(int left, int right,
+                   StringBuilder track, List<String> res) {
+        // 若左括号剩下的多，说明不合法
+        if (right < left) {
+            return;
+        }
+        // 数量小于 0 肯定是不合法的
+        if (left < 0 || right < 0) {
+            return;
+        }
+        // 当所有括号都恰好用完时，得到一个合法的括号组合
+        if (left == 0 && right == 0) {
+            res.add(track.toString());
             return;
         }
 
-        if (open < max) {
-            backtrack(ans, cur.append("("), open+1, close, max);
-            cur.deleteCharAt(cur.length() - 1);
-        }
-        if (open > close) {
-            backtrack(ans, cur.append(")"), open, close+1, max);
-            cur.deleteCharAt(cur.length() - 1);
-        }
+        // 尝试放一个左括号
+        track.append('('); // 选择
+        backtrack(left - 1, right, track, res);
+        track.deleteCharAt(track.length() - 1); // 撤消选择
+
+        // 尝试放一个右括号
+        track.append(')'); // 选择
+        backtrack(left, right - 1, track, res);
+        track.deleteCharAt(track.length() - 1); // 撤消选择
     }
 
 
