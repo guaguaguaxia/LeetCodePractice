4f6b72775c8dc049b11e5c5d3f786d7c4fba6dbd
guaguaguaxia <guaguaguaxia@Gmail.com>
2021-02-25 14:45:16 +0800

    2021-02-25

--- src/newcodetop200/part3/AddTwoNumbers.java
+++ src/newcodetop200/part3/AddTwoNumbers.java
@@ -31,7 +31,7 @@ public class AddTwoNumbers {
         ListNode pre = new ListNode(0);
         ListNode cur = pre;
         int carry = 0;
-        while(l1 != null || l2 != null) {
+        while(l1 != null || l2 != null || carry != 0) {
             int x = l1 == null ? 0 : l1.val;
             int y = l2 == null ? 0 : l2.val;
             int sum = x + y + carry;
@@ -49,9 +49,6 @@ public class AddTwoNumbers {
             if(l2 != null){
                 l2 = l2.next;
             }
-        }
-        if(carry == 1) {
-            cur.next = new ListNode(carry);
         }
         return pre.next;
     }
