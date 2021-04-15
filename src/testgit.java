c81c03bce9b2dd26d886a34073f600a1eac1eae8
guaguaguaxia <guaguaguaxia@Gmail.com>
2021-04-15 18:16:12 +0800

    2021-04-15

--- src/testgit.java
+++ src/testgit.java
@@ -1,6 +1,6 @@
 public class testgit {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("test");
+        System.out.println("dev");
+        System.out.println("dev");
     }
 }
