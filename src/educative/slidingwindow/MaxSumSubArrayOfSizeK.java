086e28fa087e4eff7329ae6522fa6fe4ba4fc9b4
zhuojian <zhuojian@cmcm.com>
2022-04-25 19:11:31 +0800

    test

--- src/educative/slidingwindow/MaxSumSubArrayOfSizeK.java
+++ src/educative/slidingwindow/MaxSumSubArrayOfSizeK.java
@@ -25,5 +25,6 @@ class MaxSumSubArrayOfSizeK {
                 + MaxSumSubArrayOfSizeK.findMaxSumSubArray(3, new int[]{2, 1, 5, 1, 3, 2}));
         System.out.println("Maximum sum of a subarray of size K: "
                 + MaxSumSubArrayOfSizeK.findMaxSumSubArray(2, new int[]{2, 3, 4, 1, 5}));
+        
     }
 }
